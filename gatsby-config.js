189e3a79d775e4631ec555c4c0352c19f709a221
Jayant Bhawal <bhawal.jayant@gmail.com>
2019-07-31 17:38:18 +0530

    Replace all tab based indentation with spaces.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,48 +5,48 @@ module.exports = {
     tagline: `A project showcasing a collection of experiments.`
   },
   plugins: [
-	  {
-		  resolve: `gatsby-plugin-sass`,
-		  options: {
-			  includePaths: ["src"],
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        includePaths: ["src"],
         cssLoaderOptions: {
           camelCase: false,
         },
-		  },
-	  }, {
-		  resolve: 'gatsby-plugin-resolve-src',
-		  options: {
-			  addSassLoader: false,
-		  },
-	  }, {
-		  resolve: `gatsby-plugin-google-fonts`,
-		  options: {
-			  fonts: [
-				  `Alegreya Sans SC\:400,500,500i`,
-				  `KoHo\:300,400,500,600`,
-				  `Lato\:300,400,700`
-			  ]
-		  }
-	  }, {
-		  resolve: `gatsby-plugin-react-css-modules`,
-		  options: {
-			  filetypes: {
-				  ".scss": { syntax: `postcss-scss` },
-			  },
-			  exclude: `\/index\/`,
-		  },
-	  }, {
-		  resolve: `gatsby-source-filesystem`,
-		  options: {
-			  name: `pages`,
-			  path: `${__dirname}/src/pages/experiments`,
-		  },
-	  },
-	  `gatsby-transformer-javascript-frontmatter`,
+      },
+    }, {
+      resolve: 'gatsby-plugin-resolve-src',
+      options: {
+        addSassLoader: false,
+      },
+    }, {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Alegreya Sans SC\:400,500,500i`,
+          `KoHo\:300,400,500,600`,
+          `Lato\:300,400,700`
+        ]
+      }
+    }, {
+      resolve: `gatsby-plugin-react-css-modules`,
+      options: {
+        filetypes: {
+          ".scss": { syntax: `postcss-scss` },
+        },
+        exclude: `\/index\/`,
+      },
+    }, {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/experiments`,
+      },
+    },
+    `gatsby-transformer-javascript-frontmatter`,
     `gatsby-plugin-typescript`,
-	  `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-tslint`,
-	  `gatsby-plugin-remove-trailing-slashes`,
-	  `gatsby-plugin-svgr`
+    `gatsby-plugin-remove-trailing-slashes`,
+    `gatsby-plugin-svgr`
   ],
 };
