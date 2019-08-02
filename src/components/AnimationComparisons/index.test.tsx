bc78f237e5ece8e0db145cee74c1fa58ce0bfaea
Jayant Bhawal <bhawal.jayant@gmail.com>
2019-08-02 13:45:06 +0530

    Add animation comparisons component.

---
+++ src/components/AnimationComparisons/index.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import AnimationComparisons from './';
+
+it('AnimationComparisons tree looks like how it should', () => {
+	let props = { id: '1' };
+
+	const component = renderer.create(<AnimationComparisons {...props} />);
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
---
+++ src/components/AnimationComparisons/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Layout from 'components/Layout';
+import css from './styles.module.scss';
+
+const AnimationComparisons = () => (
+  <div>Welcome to component AnimationComparisons!</div>
+);
+
+export default AnimationComparisons;
