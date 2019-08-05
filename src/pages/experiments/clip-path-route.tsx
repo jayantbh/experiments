d033c6f2244325a330e3516d9292ddb5acf78f37
Jayant Bhawal <bhawal.jayant@gmail.com>
2019-08-05 18:38:26 +0530

    Add placeholder clip-path page.

---
+++ src/pages/experiments/clip-path-route.tsx
@@ -0,0 +1,8 @@
+import component from 'components/Placeholder/index';
+export default component;
+
+export const frontmatter = {
+  title: 'Clip Path Route Transition',
+  description: 'Switch between multiple routes with a nice clip-path animation.',
+  categories: ['canvas', '2d', 'draw'],
+};
