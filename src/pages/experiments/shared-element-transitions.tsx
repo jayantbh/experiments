46fcd1c7a708c090d3bd6da6e500acd4d5163208
Jayant Bhawal <bhawal.jayant@gmail.com>
2019-09-02 00:46:37 +0530

    Add placeholders for two future projects.

---
+++ src/pages/experiments/shared-element-transitions.tsx
@@ -0,0 +1,8 @@
+import component from 'components/Placeholder/index';
+export default component;
+
+export const frontmatter = {
+  title: '[TODO] Shared Element Transitions',
+  description: 'Neat element transitions between multiple separate views involving similar element',
+  categories: ['web', 'shared transitions', 'css', 'animations', 'flip'],
+};
---
+++ src/pages/experiments/top-facing-cube.tsx
@@ -0,0 +1,8 @@
+import component from 'components/Placeholder/index';
+export default component;
+
+export const frontmatter = {
+  title: '[TODO] Top Facing Cube',
+  description: 'Sensors based experiment to show a 3D cube that always faces up',
+  categories: ['sensors', 'mobile', '3d'],
+};
