5f7db2df949ed2db683f757c6402bb739899bb0a
Jayant Bhawal <bhawal.jayant@gmail.com>
2019-09-01 22:23:27 +0530

    Updates

---
+++ component---src-pages-index-tsx-c5df5f4df15ee33aa7fc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{247:function(e,n,a){"use strict";a.r(n);var d=a(0),t=a.n(d);a(245);n.default=function(){return t.a.createElement("div",{className:"index-module--container--2IQ43"},t.a.createElement("div",{className:"index-module--brand--38Ksb"},"Jayant's"),t.a.createElement("div",{className:"index-module--brand-l2--2dgKz"},"experiments"))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-c5df5f4df15ee33aa7fc.js.map
